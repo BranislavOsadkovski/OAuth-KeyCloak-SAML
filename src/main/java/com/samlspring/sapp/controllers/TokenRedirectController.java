058e986e929b3c49f9bf3e41fe6cf4c43159c334
Branislav <bosadkovski@crc.ag>
2021-04-30 15:54:13 +0200

    finese

--- src/main/java/com/samlspring/sapp/controllers/TokenRedirectController.java
+++ src/main/java/com/samlspring/sapp/controllers/TokenRedirectController.java
@@ -7,6 +7,7 @@ import com.samlspring.sapp.webclient.TokenPayload;
 import com.samlspring.sapp.webclient.WebClientApi;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,10 +15,9 @@ import org.springframework.security.saml.SAMLCredential;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.ConnectException;
 import java.util.Base64;
 
 @RestController
@@ -36,22 +36,26 @@ public class TokenRedirectController {
     private final String DUMMY_STORE_AUTH_URI = "http://localhost:8090/dummy_store/authenticate/";
 
     @RequestMapping(method = RequestMethod.GET, path = "/dummy_store")
-    public void tokenRedirect(HttpServletResponse response) {
+    public void tokenRedirect(HttpServletResponse response) throws ConnectException {
 
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         LocalUser localUser = samlUserDetailsService.loadUserBySAML((SAMLCredential) auth.getCredentials());
+
         byte[] TOKEN = tokenAgent.generateNewToken();
-
         String URL_TOKEN = new String(Base64.getUrlEncoder().encode(TOKEN));
-        String POST_b64_TOKEN = new String(TOKEN);
+        String POST_TOKEN = new String(TOKEN);
 
-        webClientApi.sendData(new TokenPayload(POST_b64_TOKEN, localUser)).flux().toStream().findFirst().get();
+        ResponseEntity responseEntity = webClientApi.sendData(new TokenPayload(POST_TOKEN, localUser)).flux().toStream().findFirst().get();
 
+        if(responseEntity.getStatusCode()!= HttpStatus.OK){
+            throw new ConnectException("Payload was not delivered!");
+        }
+
+        /**
+         *      REDIRECT       / old store? / or the new store?  / extract domain from Cookie.....?
+         */
         String AUTH_REDIRECT = DUMMY_STORE_AUTH_URI + URL_TOKEN;
-/**
- * send user token
- * redirect:{URI}/{TOKEN_VALUE}
- */
+
         try {
             response.sendRedirect(AUTH_REDIRECT);
         } catch (IOException e) {
